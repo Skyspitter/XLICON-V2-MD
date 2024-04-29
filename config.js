cf719fd352c57a2320d475af163808692ce1f957
Skyspitter <167773874+Skyspitter@users.noreply.github.com>
2024-04-29 17:34:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -55,9 +55,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'Abzii'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'MASAK' 
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
